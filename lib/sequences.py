5db9869bc80e3911bb02730496b3b3b68e16efe5
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-15 22:59:59 -0400

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fib = []
+    elif length == 1:
+        fib = [0]
+    else:
+        fib = [0, 1]
+
+    while len(fib) < length:
+        new_fib = fib[-1] + fib[-2]
+        fib.append(new_fib)
+    
+    print(fib)
